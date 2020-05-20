04aae9b511f383e576c08d1272c57690b664e1ee
root <root@ip-172-31-35-8.us-east-2.compute.internal>
2020-05-20 16:02:57 +0000

    first module

--- Section2_Firststepswithtf/creates3bucket/main.tf
+++ Section2_Firststepswithtf/creates3bucket/main.tf
@@ -1,27 +1,16 @@
 resource "aws_s3_bucket" "terraform-s3" {
+  bucket = "terraform-s3-testing"
 
-bucket = "terraform-s3-testing"
+  versioning {
+    enabled = true
+  }
 
+  lifecycle {
+    prevent_destroy = true
+  }
 
-versioning {
-
-enabled = true
-
-}
-
-
-lifecycle {
-
-prevent_destroy = true
-
-}
-
-
-tags {
-
-Name = "S3 Remote store"
-
-}
-
+  tags = {
+    Name = "S3 Remote store"
+  }
 }
 
--- Section2_Firststepswithtf/createthreeinstances/main.tf
+++ Section2_Firststepswithtf/createthreeinstances/main.tf
@@ -1,18 +1,12 @@
-
 resource "aws_instance" "firstdemo" {
+  count = 3
 
-  count     = 3
-
-  ami           = "ami-922914f7"
+  ami = "ami-922914f7"
 
   instance_type = "t2.micro"
 
-
-
-  tags {
-
+  tags = {
     Name = "threedemoinstance-${count.index}"
-
   }
-
 }
+
