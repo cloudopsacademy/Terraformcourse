4a8fe5b057841333b9eff8533e96051c28eaf8cd
root <root@ip-172-31-35-8.us-east-2.compute.internal>
2020-05-20 14:06:44 +0000

    first module upgrade

--- Section2_Firststepswithtf/createfirstinstance/main.tf
+++ Section2_Firststepswithtf/createfirstinstance/main.tf
@@ -1,17 +1,10 @@
 resource "aws_instance" "firstdemo" {
-
-  ami           = "ami-922914f7"
+  ami = "ami-922914f7"
 
   instance_type = "t2.micro"
 
-
-
-  tags {
-
+  tags = {
     Name = "demoinstance"
-
   }
-
 }
-
 
