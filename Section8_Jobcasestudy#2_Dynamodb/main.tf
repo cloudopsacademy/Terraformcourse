4cd561d584f55b23f5b754fb137577f427690016
root <root@ip-172-31-35-8.us-east-2.compute.internal>
2020-05-23 17:30:07 +0000

    2_Dynamodb upgrade

--- Section8_Jobcasestudy#2_Dynamodb/main.tf
+++ Section8_Jobcasestudy#2_Dynamodb/main.tf
@@ -1,73 +1,57 @@
-
-
 resource "aws_dynamodb_table" "basic-dynamodb-t1" {
+  name = "GameScores1"
 
-name = "GameScores1"
+  read_capacity = 20
 
-read_capacity = 20
+  write_capacity = 20
 
-write_capacity = 20
+  hash_key = "UserId"
 
-hash_key = "UserId"
+  range_key = "GameTitle"
 
-range_key = "GameTitle"
+  attribute {
+    name = "UserId"
 
-attribute {
+    type = "S"
+  }
 
-name = "UserId"
+  attribute {
+    name = "GameTitle"
 
-type = "S"
+    type = "S"
+  }
 
+  attribute {
+    name = "TopScore"
+
+    type = "N"
+  }
+
+  ttl {
+    attribute_name = "TimeToExist"
+
+    enabled = false
+  }
+
+  global_secondary_index {
+    name = "GameTitleIndex"
+
+    hash_key = "GameTitle"
+
+    range_key = "TopScore"
+
+    write_capacity = 10
+
+    read_capacity = 10
+
+    projection_type = "INCLUDE"
+
+    non_key_attributes = ["UserId"]
+  }
+
+  tags = {
+    Name        = "dynamodb-table-1"
+    Environment = "production"
+  }
 }
 
-attribute {
-
-name = "GameTitle"
-
-type = "S"
-
-}
-
-attribute {
-
-name = "TopScore"
-
-type = "N"
-
-}
-
-ttl {
-
-attribute_name = "TimeToExist"
-
-enabled = false
-
-}
-
-global_secondary_index {
-
-name = "GameTitleIndex"
-
-hash_key = "GameTitle"
-
-range_key = "TopScore"
-
-write_capacity = 10
-
-read_capacity = 10
-
-projection_type = "INCLUDE"
-
-non_key_attributes = ["UserId"]
-
-}
-
-tags {
-
-Name = "dynamodb-table-1"
-
-Environment = "production"
-
-}
-
-}
