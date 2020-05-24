e998af64c89b2ec438c4c7c346788dddec8d7ec2
root <root@ip-172-31-35-8.us-east-2.compute.internal>
2020-05-24 19:40:53 +0000

    aurora upgrade

--- Section16_Jobcasestudy#7_Auroracluster/aurora.tf
+++ Section16_Jobcasestudy#7_Auroracluster/aurora.tf
@@ -1,49 +1,50 @@
-data "aws_availability_zones" "available" {}
-
+data "aws_availability_zones" "available" {
+}
 
 resource "aws_rds_cluster" "default" {
-  cluster_identifier      = "${var.CLUSTER_IDENTIFIER}"
+  cluster_identifier      = var.CLUSTER_IDENTIFIER
   engine                  = "aurora-mysql"
   engine_version          = "5.7.12"
-  availability_zones      = ["${data.aws_availability_zones.available.names}"]
-  database_name           = "${var.DATABASE_NAME}"
-  master_username         = "${var.MASTER_USERNAME}"
-  master_password         = "${var.MASTER_PASSWORD}"
+  availability_zones      = data.aws_availability_zones.available.names
+  database_name           = var.DATABASE_NAME
+  master_username         = var.MASTER_USERNAME
+  master_password         = var.MASTER_PASSWORD
   backup_retention_period = 7
   preferred_backup_window = "07:00-09:00"
   skip_final_snapshot     = "true"
   apply_immediately       = "true"
-  vpc_security_group_ids  = ["${var.VPC_SECURITY_GROUP_IDS}"]
-  db_subnet_group_name    = "${var.DB_SUBNET_GROUP_NAME}"
-  tags {
-    Name         = "${var.ENVIRONMENT_NAME}-Aurora-DB-Cluster"
-    ManagedBy    = "${var.MANAGER}"
-    Environment  = "${var.ENVIRONMENT_NAME}"
+  vpc_security_group_ids  = [var.VPC_SECURITY_GROUP_IDS]
+  db_subnet_group_name    = var.DB_SUBNET_GROUP_NAME
+  tags = {
+    Name        = "${var.ENVIRONMENT_NAME}-Aurora-DB-Cluster"
+    ManagedBy   = var.MANAGER
+    Environment = var.ENVIRONMENT_NAME
   }
-
-
 }
 
 resource "aws_rds_cluster_instance" "cluster_instances" {
-  count              = "${var.INSTANCE_COUNT_IN_CLUSTER}"
+  count              = var.INSTANCE_COUNT_IN_CLUSTER
   identifier         = "${var.CLUSTER_IDENTIFIER}-${count.index}"
-  cluster_identifier = "${aws_rds_cluster.default.id}"
-  instance_class     = "${var.DB_INSTANCE_CLASS}"
+  cluster_identifier = aws_rds_cluster.default.id
+  instance_class     = var.DB_INSTANCE_CLASS
   engine             = "aurora-mysql"
 
-  tags {
-    Name         = "${var.ENVIRONMENT_NAME}-aurora-instance-${count.index}"
-    ManagedBy    = "${var.MANAGER}"
-    Environment  = "${var.ENVIRONMENT_NAME}"
+  tags = {
+    Name        = "${var.ENVIRONMENT_NAME}-aurora-instance-${count.index}"
+    ManagedBy   = var.MANAGER
+    Environment = var.ENVIRONMENT_NAME
   }
 }
 
-output "Cluster Endpoint" {
-  value = "${aws_rds_cluster.default.endpoint}"
+output "Cluster_Endpoint" {
+  value = aws_rds_cluster.default.endpoint
 }
-output "Cluster Instance Endpoints" {
-  value = "${aws_rds_cluster_instance.cluster_instances.*.endpoint}"
+
+output "Cluster_Instance_Endpoints" {
+  value = aws_rds_cluster_instance.cluster_instances.*.endpoint
 }
-output "Reader Endpoint" {
-  value = "${aws_rds_cluster.default.reader_endpoint}"
+
+output "Reader_Endpoint" {
+  value = aws_rds_cluster.default.reader_endpoint
 }
+
--- Section16_Jobcasestudy#7_Auroracluster/variable.tf
+++ Section16_Jobcasestudy#7_Auroracluster/variable.tf
@@ -1,17 +1,39 @@
+variable "AWS_ACCESS_KEY" {
+}
 
+variable "AWS_SECRET_KEY" {
+}
 
-variable "AWS_ACCESS_KEY" {}
-variable "AWS_SECRET_KEY" {}
-variable "AWS_REGION" {}
+variable "AWS_REGION" {
+}
 
+variable "CLUSTER_IDENTIFIER" {
+}
 
-variable "CLUSTER_IDENTIFIER" {}
-variable "DB_INSTANCE_CLASS" {}
-variable "MASTER_USERNAME" {}
-variable "MASTER_PASSWORD" {}
-variable "DATABASE_NAME" {}
-variable "INSTANCE_COUNT_IN_CLUSTER" {}
-variable "ENVIRONMENT_NAME" {}
-variable "MANAGER" {}
-variable "DB_SUBNET_GROUP_NAME" {}
-variable "VPC_SECURITY_GROUP_IDS"{}
+variable "DB_INSTANCE_CLASS" {
+}
+
+variable "MASTER_USERNAME" {
+}
+
+variable "MASTER_PASSWORD" {
+}
+
+variable "DATABASE_NAME" {
+}
+
+variable "INSTANCE_COUNT_IN_CLUSTER" {
+}
+
+variable "ENVIRONMENT_NAME" {
+}
+
+variable "MANAGER" {
+}
+
+variable "DB_SUBNET_GROUP_NAME" {
+}
+
+variable "VPC_SECURITY_GROUP_IDS" {
+}
+
