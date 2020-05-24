94d280aed4595ad5b49945efcab8629c7f8e13d7
root <root@ip-172-31-35-8.us-east-2.compute.internal>
2020-05-24 20:07:23 +0000

    AnsibleTerraform

--- AnsibleandTerraform/firstinstance.tf
+++ AnsibleandTerraform/firstinstance.tf
@@ -1,24 +1,18 @@
-
 resource "aws_instance" "firstdemo" {
-  ami           = "ami-922914f7"
-  instance_type = "t2.micro"
+  ami             = "ami-922914f7"
+  instance_type   = "t2.micro"
   security_groups = ["demo-sg"]
-  key_name = "newck"
-  tags {
+  key_name        = "newck"
+  tags = {
     Name = "demoinstance1"
   }
 }
 
-
 resource "null_resource" "provisioning" {
+  depends_on = [aws_instance.firstdemo]
 
-depends_on = ["aws_instance.firstdemo"]
-
-provisioner "local-exec" {
-        command = "sleep 120 ; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ec2-user --private-key ./newck.pem -i '${aws_instance.firstdemo.public_ip},' nginxplay.yml" }
-
+  provisioner "local-exec" {
+    command = "sleep 120 ; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ec2-user --private-key ./newck.pem -i '${aws_instance.firstdemo.public_ip},' nginxplay.yml"
+  }
 }
-
-
-
 
